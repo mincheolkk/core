5bff9e2c427a2ace21cbb88bb05e38b86772dde0
mincheolkk <kmc04080408@gmail.com>
2021-06-28 14:55:03 +0900

    MOD : AppConfig 리팩토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,15 +12,29 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig  {
 
+//    public MemberService memberService() {
+//        return new MemberServiceImpl(new MemoryMemberRepository()); // 생성자를 통해서 객체가 들어감 (생성자 주입)
+//    }
+    /** 구현체인 MemoryMemberRepository 에선 MemberRepository 라는 역할이 잘 안 보임. 아래처럼 리팩토링 **/
+    
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 생성자를 통해서 객체가 들어감 (생성자 주입)
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    /*
-    이제는 MemberServiceImpl 과  OrderServiceImpl 을 AppConfig 에서 생성성
-    */
+     public DiscountPolicy discountPolicy() {
+         return new FixDiscountPolicy();
+//         return new RateDiscountPolicy();
+     }
+
+    /**
+    이제는 MemberServiceImpl 과  OrderServiceImpl 을 AppConfig 에서 생성
+    **/
 }
