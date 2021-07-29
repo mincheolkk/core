a9377e7cc9e8172053979ee3cf3c66922b4df12e
mincheol <kmc04080408@gmail.com>
2021-07-29 19:14:41 +0900

    Mod : 프록시 추가

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -14,19 +14,20 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
-    // MyLogger 를 찾을 수 있는 DL 할 수 있는 애가 주입됨 .
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody // 문자열을 그대로 보낼 수 있음 .
     public String logDemo(HttpServletRequest request) {
-        MyLogger myLogger = myLoggerProvider.getObject();
-        // 처음 MyLogger 가 사용됨 .
         String requestURL = request.getRequestURL().toString();
+
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
         logDemoService.logic("testId");
         return "ok";
     }
+
+    // provider 를 사용하든, 프록시를 사용하든 핵심 아이디어는 진짜 객체 조회를 꼭 필요한 시점까지 지연처리 한다는 점 .
 }
